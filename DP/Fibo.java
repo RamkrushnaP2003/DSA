ce9ca2d4a252b6daa8d72e5825bb4006d78caedb
Ramkrushna Parkipandla <ramkrushnaparkipandla2003@gmail.com>
2025-06-19 20:49:34 +0530

    Added: edge case for opt solution

--- DP/Fibo.java
+++ DP/Fibo.java
@@ -27,6 +27,7 @@ public class Fibo {
     }
 
     public static int fibo4(int n) {
+        if(n<=1) return n;
         int prev = 0, curr = 1;
         for(int i=2; i<=n; i++) {
             int next = prev + curr;
