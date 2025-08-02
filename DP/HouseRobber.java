929897001109a170dda6a7a7296a9089ed8ef3f4
Ramkrushna Parkipandla <ramkrushnaparkipandla2003@gmail.com>
2025-08-02 22:52:11 +0530

    Refactor houseRobber method and add maxSumT and maxSumSO for optimized calculations

--- DP/HouseRobber.java
+++ DP/HouseRobber.java
@@ -6,7 +6,10 @@ public class HouseRobber {
         int dp2[] = new int[nums.length+1];
         Arrays.fill(dp1, -1);
         Arrays.fill(dp2, -1);
-        return Math.max(maxSum(nums, dp1, nums.length-1, true), maxSum(nums, dp2, nums.length-2, false));
+        // int ans = Math.max(maxSum(nums, dp1, nums.length-1, true), maxSum(nums, dp2, nums.length-2, false));
+        int ans = Math.max(maxSumT(nums, 0, nums.length-1), maxSumT(nums, 1, nums.length));
+        // int ans = Math.max(maxSumSO(nums, 0, nums.length-1), maxSumSO(nums, 1, nums.length));
+        return ans;
     }
 
     public static int maxSum(int nums[], int dp[], int n, boolean isFirst) {
@@ -23,8 +26,34 @@ public class HouseRobber {
         return dp[n] = Math.max(nonPick, pick);
     }
 
+    public static int maxSumT(int nums[], int st, int end) {
+        int dp[] = new int[end];
+        Arrays.fill(dp, -1);
+        dp[st] = nums[st];
+        for(int i=st+1; i<end; i++) {
+            int pick = nums[i];
+            if(i > st+1) pick += dp[i-2];
+            int nonPick = dp[i-1];
+            dp[i] = Math.max(pick, nonPick);
+        }
+        return dp[end-1];
+    }
+
+    public static int maxSumSO(int nums[], int st, int end) {
+        int n=end, prev1=0, prev2=nums[st], curr=0;
+        for(int i=st+1; i<end; i++) {
+            int pick = nums[i];
+            if(i > st+1) pick += prev2;
+            int nonPick = prev1;
+            curr = Math.max(pick, nonPick);
+            prev1 = prev2;
+            prev2 = curr;
+        }
+        return prev1;
+    }
+
     public static void main(String[] args) {
-        int nums[] = {1, 2, 3, 4, 5};
+        int nums[] = {1, 2, 3, 4, 5, 6};
         System.out.println(houseRobber(nums));
     }
 }
