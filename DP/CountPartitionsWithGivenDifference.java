b810e756cdbe482b8900eb39af1226c18b8b00c2
Ramkrushna Parkipandla <ramkrushnaparkipandla2003@gmail.com>
2025-10-27 00:16:04 +0530

    .

--- DP/CountPartitionsWithGivenDifference.java
+++ DP/CountPartitionsWithGivenDifference.java
@@ -21,8 +21,8 @@ public class CountPartitionsWithGivenDifference {
         }
         if(dp[ind][target]!=-1) return dp[ind][target];
         int pick = 0;
-        if(nums[ind]<=target) pick = countPartitionsWithGivenDifference(nums, ind-1, target-nums[ind]);
-        int nonPick = countPartitionsWithGivenDifference(nums, ind-1, target);
+        if(nums[ind]<=target) pick = countPartitionsWithGivenDifference(nums, ind-1, target-nums[ind], dp);
+        int nonPick = countPartitionsWithGivenDifference(nums, ind-1, target, dp);
         return dp[ind][target] = pick + nonPick;
     }
 
@@ -34,15 +34,15 @@ public class CountPartitionsWithGivenDifference {
         } else {
             dp[0][0] = 1;
         } 
-        if(nums[0]!=0 && nums[0]==target) { // nums[0]!=0 convert dp[0][nums[0]] = 2 -> 1 then will give wrong ans 
+        if(nums[0]!=0 && nums[0]<=target) { // nums[0]!=0 convert dp[0][nums[0]] = 2 -> 1 then will give wrong ans 
             dp[0][nums[0]] = 1;
         }
         for(int i=1; i<n; i++) {
-            for(int j=1; j<=target; j++) {
+            for(int j=0; j<=target; j++) {
                 int pick = 0;
                 if(nums[i]<=j) pick = dp[i-1][j-nums[i]];
                 int nonPick = dp[i-1][j];
-                dp[i][target] = pick + nonPick;
+                dp[i][j] = pick + nonPick;
             }
         }
         return dp[n-1][target];
@@ -52,11 +52,23 @@ public class CountPartitionsWithGivenDifference {
         int nums[] = {1, 1, 1, 1}, d=0; // 0 <= nums[i] <= 6 && 1 <= nums.length <= 50
         int totalSum = 0;
         for(int i: nums) totalSum += i;
+        /*
+         * totalSum = s1 - s2
+         * s1 - s2 = D
+         * totalSum - s2 - s2 = D    /. s1 = totalSum - s2
+         * totalSum - 2 * s2 = D
+         * totalSum - D = 2 * s2
+         * s2 = ( totalSum - D ) / 2;
+        */
         int target = (totalSum - d) / 2;
-        int dp[][] = new int[nums.length][target];
+        int dp[][] = new int[nums.length][target+1];
         for(int i[]: dp) Arrays.fill(i, -1);
         System.out.println(countPartitionsWithGivenDifference(nums, nums.length-1, target));
-        System.out.println(countPartitionsWithGivenDifference(nums, nums.length-1, target));
-        System.out.println(countPartitionsWithGivenDifference(nums, nums.length-1, target));
+        System.out.println(countPartitionsWithGivenDifference(nums, nums.length-1, target, dp));
+        if((totalSum - d) < 0 || (totalSum - d)%2==1) {
+            System.out.println(-1);
+            return;
+        }
+        System.out.println(countPartitionsWithGivenDifference(nums, target));
     }
 }
